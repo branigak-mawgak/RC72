8f7a0a47b380de16986b7b800b21a465b7d75cf8
branigak-mawgak <branigak@gmail.com>
2026-08-28 15:27:57 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
-const CACHE="rc72-final-v3";
+const CACHE="rc72-v2";
 const CORE=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>{const u=new URL(e.request.url); if(u.origin!==location.origin)return; if(e.request.method!=="GET")return; e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(ca=>ca.put(e.request,c));return r}).catch(()=>caches.match(e.request)));});
+self.addEventListener("fetch",e=>{
+ const u=new URL(e.request.url);
+ if(u.origin===location.origin){
+  e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{
+   const copy=r.clone(); caches.open(CACHE).then(x=>x.put(e.request,copy)); return r;
+  })));
+ }
+});
