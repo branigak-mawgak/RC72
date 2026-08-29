95d4fc34063cb95b2b2efd67b0d426c0ae9d6f7f
branigak-mawgak <branigak@gmail.com>
2026-08-29 10:13:15 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rc71-pwa-pdf-r2';
+const CACHE='rc71-pwa-pdf-r3';
 const CORE=['./','./index.html','./manifest.webmanifest','./YASINAN.pdf','./WIRIDAN(1).pdf'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('rc71-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
